7463b7e4a3b861ba826868e4ab1d261aca0fe12a
joe <joe@lite-check.com>
2026-05-04 07:53:58 -0700

    iOS actuals created. Double the actual for the worst iOS sqlDelite ever.
    It compiles at least.

---
+++ composeApp/src/iosArm64Main/kotlin/com/worstprogrammer/worstkmp/Platform.iosArm64.kt
@@ -0,0 +1,10 @@
+package com.worstprogrammer.worstkmp
+
+import platform.UIKit.UIDevice
+
+actual fun getPlatform(): Platform = IOSPlatform()
+
+class IOSPlatform : Platform {
+    override val name: String
+        get() = "${UIDevice.currentDevice.systemName()} ${UIDevice.currentDevice.systemVersion}"
+}
