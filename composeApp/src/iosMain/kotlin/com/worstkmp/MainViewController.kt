f0dea4ee4094a5cec5201f540a38692598936fb1
joe <joe@lite-check.com>
2026-05-14 09:33:28 -0700

    Added koin to iOS

--- composeApp/src/iosMain/kotlin/com/worstkmp/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/worstkmp/MainViewController.kt
@@ -2,4 +2,12 @@ package com.worstprogrammer.worstkmp
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    if (GlobalContext.getOrNull() == null) {
+        startKoin {
+            modules(appModule)
+        }
+    }
+
+    App()
+}
