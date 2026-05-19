01224dfd837d99326b878e0ff48759bbccf25b21
joe <joe@lite-check.com>
2026-05-19 10:36:43 -0700

    Added some future disabled features of the worst kind.

--- composeApp/src/commonMain/kotlin/com/worstkmp/navigation/AppNavigation.kt
+++ composeApp/src/commonMain/kotlin/com/worstkmp/navigation/AppNavigation.kt
@@ -4,6 +4,10 @@ import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
 import com.worstkmp.ui.screens.HomeScreen
 
+/**
+ * The root of the app's navigation graph.
+ * This wraps the whole app in a [Navigator] for use with Voyager navigation.
+ */
 @Composable
 fun AppNavigation() {
     Navigator(screen = HomeScreen())
