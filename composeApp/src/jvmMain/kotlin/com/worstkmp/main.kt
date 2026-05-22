40c5bf3b8f3de058e6563012ed6ea28294ca9e8f
Joe H <4879956+MightyJoe@users.noreply.github.com>
2026-05-22 14:11:58 -0700

    made changes to try to dereference koin for now.

--- composeApp/src/jvmMain/kotlin/com/worstkmp/main.kt
+++ composeApp/src/jvmMain/kotlin/com/worstkmp/main.kt
@@ -2,14 +2,8 @@ package com.worstkmp
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import com.worstkmp.di.appModule
-import org.koin.core.context.startKoin
 
 fun main() = application {
-    // Start Koin for Desktop
-    startKoin {
-        modules(appModule)
-    }
 
     Window(
         onCloseRequest = ::exitApplication,
